f3651b46762326c0c93f2e90cd136f5ead3bd58e
alisher-zinullayev <zinullayev09@gmail.com>
2023-07-17 13:03:48 +0600

    Initial Commit

---
+++ MeetPoint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeetPoint
+//
+//  Created by Alisher Zinullayev on 17.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
